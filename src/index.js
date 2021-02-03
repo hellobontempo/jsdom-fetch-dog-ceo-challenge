d32dae677c4d5b6e95f7874e6b44bf60f5d30ff0
claire <clairebontempo@gmail.com>
2021-02-02 16:35:09 -0800

    adds images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+fetchImages()
+function fetchImages() {
+    fetch(imgUrl)
+        .then(resp => resp.json())
+            .then(data => {
+                addImgToDom(data)
+            })
+}
+
+function addImgToDom(data) {
+    const imgArray = data["message"]
+    imgArray.forEach(image =>{
+        const dogImageContainer = document.querySelector("#dog-image-container")
+        let img = document.createElement('img');
+        img.src = image
+        dogImageContainer.appendChild(img)
+    })
+}
+
+//   function renderBooks(books) {
+//     const main = document.querySelector('main')
+//     books.forEach(book => {
+//       const h2 = document.createElement('h2')
+//       h2.innerHTML = book.name
+//       main.appendChild(h2)
+//     })
+//   }
+  
